d4da193a776e35d507f317a3ac03902edb9a1657
daftshady <daftonshady@gmail.com>
2016-10-11 02:09:37 +0900

    initializing js module environments

---
+++ dropbeat/static/js/dropbeat.js
@@ -0,0 +1,25 @@
+'use strict';
+
+define([
+  'jquery'
+], function($) {
+
+function Dropbeat() {
+}
+
+/*
+ * The core Dropbeat client
+ *
+ * @this {Dropbeat}
+ */
+
+Dropbeat.prototype = {
+  debug: true,
+
+  initialize: function() {
+  }
+};
+
+return new Dropbeat();
+
+});
---
+++ dropbeat/static/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require.config({
+  baseUrl: 'js',
+  paths: {
+    jquery: 'lib/jquery-3.1.1.min'
+  }
+
+});
+
+require([
+  'dropbeat',
+
+], function(Dropbeat) {
+  Dropbeat.initialize();
+});
