2d6369811da7b4bbbbb79f2d05622b3e2006236d
daftshady <daftonshady@gmail.com>
2016-10-31 22:33:28 +0900

    domready

--- dropbeat/static/js/main.js
+++ dropbeat/static/js/main.js
@@ -4,16 +4,17 @@ require.config({
   baseUrl: 'js',
   paths: {
     jquery: 'lib/jquery-3.1.1.min',
-    handlebars: 'lib/handlebars-4.0.5'
+    handlebars: 'lib/handlebars-4.0.5',
+    domReady: 'lib/domReady'
   }
 
 });
 
 require([
-  'dropbeat', 'jquery'
+  'domReady', 'dropbeat', 'jquery'
 
-], function (Dropbeat, $) {
-  $(document).ready(Dropbeat.initialize);
+], function (domReady, Dropbeat, $) {
+  domReady(Dropbeat.initialize);
 });
 
 // eslint-disable-next-line no-unused-vars
